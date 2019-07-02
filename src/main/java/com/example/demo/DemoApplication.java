8e3e1ff895cd5e9ca69b0aa2a0c494772ea04abd
sunxiaoyu <sunxiaoyu@yuuwei.com>
2019-07-02 21:18:00 +0800

    create project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author sxy
+ * @Date 19-7-2 下午4:07
+ * @Version 1.0
+ **/
+@MapperScan("com.example.demo.mapper")
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("hello");
+    }
+
+}
